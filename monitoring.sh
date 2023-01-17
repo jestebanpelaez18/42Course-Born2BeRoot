ddd592cdd40ce72e2d1c7ac20948627c5997eea0
Juan Esteban Pelaez Hoyos <jpelaez-@c2r5p3.hive.fi>
2023-01-17 15:41:00 +0200

    2

--- monitoring.sh
+++ monitoring.sh
@@ -4,8 +4,8 @@ wall "  #Architecture: $(uname -a)
         #CPU physical: $(cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc -l)
         #vCPU: $(cat /proc/cpuinfo | grep processor | wc -l)
         #Memory Usage: $(free | grep Mem | awk '{printf("%d/%dMB (%.2f)", $3 / 1000, $2 / 1000, $3/$2 * 100.0)}')
-        #Disk Usage: 
-        #CPU load:
+        #Disk Usage: $(df -h --total | grep total | awk '{printf("%d/%dGb (%.2f)", $3, $2, $3/$2 * 100.0)}')
+        #CPU load: $(top -bn2 | grep Cpu | awk '{printf("%.2f%%", 2$ + 4$)}')
         #Last boot: $(who -b | awk '{print $3,$4}' )
         #LVM use: 
         #Connections TCP: $(netstat -nat | grep | awk '{print $6}')
